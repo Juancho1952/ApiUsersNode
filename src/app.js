ac9873262fcf2d8d2ce8270959b903b0b56e9345
juancho1952 <juancho1952@gmail.com>
2022-12-06 19:31:38 -0500

    Se hace actualización del entregable 3 con todos los metodos operativos para la posterior entrega al docente

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const db = require('./utils/database')
+const usersRoute = require('./users/users.router')
+
 
 const port = 9000
+const app = express()
 
-const app = express
+app.use(express.json())
+
+db.authenticate()
+    .then(() => console.log('Database Autheticated'))
+    .catch((err) => console.log(err))
+
+db.sync()
+    .then(() => console.log('Database Sync'))
+    .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'Ok!'})
-}) 
+    res.status(200).json({ message: 'Ok!' })
+})
 
-app.listen(() => {
+app.use('/api/v1', usersRoute)
+
+
+app.listen(port, () => {
     console.log(`Server started at port ${port}`)
 })
